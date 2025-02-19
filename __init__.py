ff44cbebd3c4557289199bd389b0d0d5746c6a34
bdeshi <bdeshi@bdeshi.space>
2025-02-19 18:27:13 +0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os
 
-GH_ACCESS_TOKEN = os.getenv("GH_ACCESS_TOKEN", "ghp_BDdGw3SGMA9Wyk5bD1qV574pAMoDsX2b3sus")
+GH_ACCESS_TOKEN = os.getenv("GH_ACCESS_TOKEN", "")
 
 if __name__ == "__main__":
     print(f"using the access token {GH_ACCESS_TOKEN}")
