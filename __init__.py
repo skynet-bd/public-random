c383be5342cc95d279289710ecccf076aa6881c0
bdeshi <bdeshi@bdeshi.space>
2025-02-21 00:01:40 +0600

    add secret test sarif path in properties file

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import os
 
 GH_ACCESS_TOKEN = os.getenv("GH_ACCESS_TOKEN", "ghp_BDdGw3SGMA9Wyk5bD1qV574pAMoDsX2b3sus")
+GH_ACCESS_TOKEN2 = os.getenv("GH_ACCESS_TOKEN", "ghp_BDdGw3SGMA9Wyk5bD1qV574pAMoDsX2b2sus")
 
 if __name__ == "__main__":
     print(f"using the access token {GH_ACCESS_TOKEN}")
