a7437d6f57c8694661a225b8d695d889677da6a6
bdeshi <bdeshi@bdeshi.space>
2025-02-21 00:32:43 +0600

    branch for pr

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 import os
 
-GH_ACCESS_TOKEN = os.getenv("GH_ACCESS_TOKEN", "ghp_BDdGw3SGMA9Wyk5bD1qV574pAMoDsX2b0sus")
+GH_ACCESS_TOKEN1 = os.getenv("GH_ACCESS_TOKEN", "ghp_BDdGw3SGMA9Wyk5bD1qV574pAMoDsX2b1sus")
 GH_ACCESS_TOKEN2 = os.getenv("GH_ACCESS_TOKEN", "ghp_BDdGw3SGMA9Wyk5bD1qV574pAMoDsX2b2sus")
 GH_ACCESS_TOKEN3 = os.getenv("GH_ACCESS_TOKEN", "ghp_BDdGw3SGMA9Wyk5bD1qV574pAMoDsX2b3sus")
+GH_ACCESS_TOKEN4 = os.getenv("GH_ACCESS_TOKEN", "ghp_BDdGw3SGMA9Wyk5bD1qV574pAMoDsX2b4sus")
 
 if __name__ == "__main__":
-    print(f"using the access token {GH_ACCESS_TOKEN}")
+    print(f"using the access token {GH_ACCESS_TOKEN1}")
